cbe3bfbe25a6284ccdc239d0e670fc4373f079b1
Bogdan Dolin <d.bogdan@inbox.ru>
2017-06-13 23:40:48 +0700

    progress catalog page

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,2 +1,13 @@
 $(document).ready(function () {
+  var $navTrigger = $('.nav-trigger');
+  var $mobileMenu = $('.mobile-menu');
+  var $outWrapper = $('.out');
+
+  // Mobile menu click events
+  $('.nav-trigger').click(function (e) {
+    e.preventDefault();
+    $navTrigger.toggleClass('js-active');
+    $mobileMenu.toggleClass('js-active');
+    $outWrapper.toggleClass('js-overlayed');
+  });
 });
